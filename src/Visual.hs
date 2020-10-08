0f4d94eb284e0b3d9752705111c65581b772f184
flupe <lucas@escot.me>
2020-10-08 13:35:00 +0200

    added grid for gallery

--- src/Visual.hs
+++ src/Visual.hs
@@ -5,40 +5,50 @@ import Common
 import Templates (outerWith, loading_)
 import Lucid
 
+
+thumbWidth :: Int
+thumbWidth = 710
+
+
 build :: Task IO ()
 build = do
     pictures <- match "visual/*" \src -> do
-      copyFile src
-      src' <- toAbsolute src
-      size <- (resized . read)
-              <$> readCommand "identify" ["-ping", "-format", "(%w, %h)", src']
-      processMagick src
-        <&> timestamped
-        <&> fmap (size,) 
+        copyFile src
+        callCommandWith
+            (\a b -> "convert " <> a <> " " <> b)
+            (-<.> "webp")
+            src
+        src' <- toAbsolute src
+        size <- (resized . read)
+                <$> readCommand "identify" ["-ping", "-format", "(%w, %h)", src']
+        callCommandWith
+            (\a b -> "convert -resize 710x " <> a <> " " <> b)
+            (-<.> "thumb.webp")
+            src
+            <&> timestamped
+            <&> fmap (size,) 
 
     watch pictures $ match_ "./visual.rst" \src -> do
         intro <- compilePandoc src
         write "visual.html" $ renderVisual intro (recentFirst pictures)
-
-resized :: (Int, Int) -> (Int, Int)
-resized (width, height) = (710, round $ fi height * 710.0 / fi width)
     where
-        fi :: Int -> Float
-        fi = fromIntegral
+        resized :: (Int, Int) -> (Int, Int)
+        resized (width, height) =
+            (thumbWidth, round $ fi height * fi thumbWidth / fi width)
+            where
+                fi :: Int -> Float
+                fi = fromIntegral
+
 
 renderVisual :: Text -> [Timestamped ((Int, Int), FilePath)] -> Html ()
 renderVisual txt imgs =
     outerWith def {title = "visual"} do
         toHtmlRaw txt
         hr_ []
-        section_ $ forM_ imgs \ (Timestamped _ ((width, height), p)) ->
-            figure_ $ img_
-                [ src_ (fromString p)
-                , width_ (fromString $ show width)
-                , height_ (fromString $ show height)
-                , loading_ "lazy" ]
-
-processMagick :: FilePath -> Task IO FilePath
-processMagick = callCommandWith
-    (\a b -> "convert -resize 710x " <> a <> " " <> b)
-    (-<.> "thumb.webp")
+        section_ [class_ "visual"] $
+            forM_ imgs \ (Timestamped _ ((width, height), p)) ->
+                figure_ $ a_ [href_ (fromString (replaceExtensions p "webp"))] $ img_
+                    [ src_ (fromString p)
+                    , width_ (fromString $ show width)
+                    , height_ (fromString $ show height)
+                    , loading_ "lazy" ]
